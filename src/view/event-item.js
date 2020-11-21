09db6c6f7d57b94776217144972b82c0e4178fd7
MaksBmt <solov@tdpsm.ru>
2020-11-21 17:40:32 +0200

    исправляет ошибку по требованию линтера

--- src/view/event-item.js
+++ src/view/event-item.js
@@ -1,5 +1,5 @@
 export const createEventItem = (point) => {
-  const {type, city, price, offers} = point;
+  const {type, city, price} = point;
   return (`
   <li class="trip-events__item">
               <div class="event">
