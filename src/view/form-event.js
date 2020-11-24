129d7dda7613638d67907f0125e32fdcdd777124
MaksBmt <solov@tdpsm.ru>
2020-11-24 14:19:34 +0200

    вносит изменения в форму по наименованиям офферсов

--- src/view/form-event.js
+++ src/view/form-event.js
@@ -9,14 +9,18 @@ const creationListDistanation = () => {
   );
 };
 
+const receivingEndElement = (sentence) => {
+  return sentence.split(` `).pop();
+};
+
 const creationListOffers = (offersForm) => {
   return (`
   <section class="event__section  event__section--offers">
     <h3 class="event__section-title  event__section-title--offers">Offers</h3>
     <div class="event__available-offers">
        ${offersForm.map((offerForm) => `<div class="event__offer-selector">   
-      <input class="event__offer-checkbox  visually-hidden" id="event-offer-luggage-1" type="checkbox" name="event-offer-luggage">
-        <label class="event__offer-label" for="event-offer-luggage-1">
+      <input class="event__offer-checkbox  visually-hidden" id="event-offer-${receivingEndElement(offerForm.name)}-1" type="checkbox" name="event-offer-${receivingEndElement(offerForm.name)}">
+        <label class="event__offer-label" for="event-offer-${receivingEndElement(offerForm.name)}-1">
           <span class="event__offer-title">${offerForm.name}</span>
         &plus;&euro;&nbsp;
         <span class="event__offer-price">${offerForm.price}</span>
